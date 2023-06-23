ecff2649ec6a0ab46e10d818f0a4e93b7b4e26b4
Steve <steve.craig@outlook.com>
2023-06-23 16:30:39 +0100

    New Icons

--- CVHA Editor/CVHA_EditorDocument.swift
+++ CVHA Editor/CVHA_EditorDocument.swift
@@ -17,7 +17,73 @@ extension UTType {
 struct CVHA_EditorDocument: FileDocument {
     var text: String
 
-    init(text: String = "Hello, archers!") {
+    init(text: String = """
+---
+layout: post.njk
+title: Your Post Title
+tags: ['Post']
+description: Edit this to give a post description
+featuredImage: /_images/cvha-badge-default.png
+date: 2023-07-24
+---
+
+Edit the rest of this to put in what you want.
+
+# Title H1
+## Title H2
+### Title H3
+#### Title H4
+##### Title H5
+###### Title H6
+
+## Paragraphs
+
+Note: Bulma CSS is the main formatting engine for the site
+
+Etiam ut risus sagittis, posuere mi at, congue dui. Sed in turpis ut felis bibendum blandit non sit amet ipsum. Etiam mattis metus sed felis cursus ultricies. Donec varius lectus maximus orci mattis sagittis.
+
+Donec quis turpis eu justo suscipit eleifend in at mi. Nulla at lectus molestie, blandit elit eu, eleifend sem. Nunc suscipit tincidunt consectetur.
+
+## Bold, cursive and link
+**Bold**, _cursive_ and [link](https://www.11ty.dev/)
+
+## Lists
+
+- Donec quis turpis eu justo suscipit eleifend in at mi.
+  - Nulla at lectus molestie, blandit elit eu, eleifend sem.
+  - Nunc suscipit tincidunt consectetur.
+- Integer et neque in purus pulvinar consequat vel quis arcu.
+- Morbi porttitor blandit justo quis commodo.
+
+1. Element 1
+2. Element 2
+3. Element 3
+4. Element 4
+
+## Images
+![imagen](/_images/ClydeValleyHorseArchersMain.png)
+
+
+## Table
+
+| Item         | Price     | # In stock |
+|--------------|-----------|------------|
+| Juicy Apples | 1.99      | *7*        |
+| Bananas      | **1.89**  | 5234       |
+
+## Quotes
+
+> Donec quis turpis eu justo suscipit eleifend in at mi. Nulla at lectus molestie, blandit elit eu, eleifend sem. Nunc suscipit tincidunt consectetur.
+
+## Code
+
+`console.log('Hello World')`
+
+```
+// tag <pre>
+console.log('Hello World')
+```
+""") {
         self.text = text
     }
 
